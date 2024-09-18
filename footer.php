6747b256d74b528e88599f28c2764b43ed99aba7
cpdev04 <159432795+cpdev04@users.noreply.github.com>
2024-09-18 12:32:10 +0530

    commit

---
+++ footer.php
@@ -0,0 +1,124 @@
+<footer>
+      <section class="footer-bg">
+         <div class="footer-area">
+            <div class="container">
+               <div class="footer-grid b-b">
+                  <div class="footer-widget-2 footer-col-1">
+                     <div class="footer-logo mb-35">
+                        <a href="index.html">
+                           <img src="assets/imgs/logo/slvlogo.jpg" alt="image bnot found"
+                              style="width: 120px !important;">
+                        </a>
+                     </div>
+
+                     <div class="footer-widget-info">
+                        <div class="footer-info mb-35">
+                           <div class="footer-info-item d-flex align-items-start">
+                              <div class="footer-info-icon mr-20">
+                                 <span> <i class="fa-solid fa-location-dot"></i></span>
+                              </div>
+   
+                              <div class="footer-info-text">
+                                 <a href="#">Kalppa Virusha hospital
+RG Arcade, Sarjapur Rd,
+Anekal Taluk, Attibele,
+Anekal Taluk, Attibele,
+Bangalore-562107</a>
+                              </div>
+   
+                           </div>
+                           <div class="footer-info-item d-flex align-items-start">
+                              <div class="footer-info-icon mr-20">
+                                 <span><i class="fa-solid fa-envelope"></i></span>
+                              </div>
+   
+                              <div class="footer-info-text">
+                                 <a href="mailto:example@mail.com">example@mail.com</a>
+                              </div>
+   
+                           </div>
+                           <div class="footer-info-item d-flex align-items-start">
+                              <div class="footer-info-icon mr-20">
+                                 <span><i class="fa-solid fa-phone"></i></span>
+                              </div>
+   
+                              <div class="footer-info-text">
+                                 <a href="tel:08042042435">08042042435</a>
+                                 <a href="tel:+918861796435">+918861796435</a>
+                              </div>
+                           </div>
+                        </div>
+                        <div class="theme-social">
+                           <a href="https://www.facebook.com/hospital.slvprasad/"><i class="fa-brands fa-facebook-f"></i></a>
+   
+   
+                           <a href="https://www.instagram.com/slv_prasad_hospital/"><i class="fa-brands fa-instagram"></i></a>
+   
+                           <a href="https://www.linkedin.com/company/slv-prasad-hospital/"><i class="fa-brands fa fa-linkedin"></i></a>
+                        </div>
+                     </div>
+
+                  </div>
+
+                 
+
+                  <div class="footer-widget-2 footer-col-2">
+                     <div class="footer-widget-title">
+                        <h4>Department</h4>
+                     </div>
+                     <div class="footer-link">
+                        <ul>
+                           <li style="white-space: nowrap;"><a href="#">Obestric Gyneacology</a></li>
+                           <li><a href="service.html">General Medicine</a></li>
+                           <li><a href="service.html">General Surgeon</a></li>
+                           <li><a href="service.html">Pediatrician</a></li>
+                           <li><a href="service.html">Ortho Surgeon</a></li>
+                           <li><a href="service.html">...View More...</a></li>
+                        </ul>
+                     </div>
+                  </div>
+                  <div class="footer-widget-2 footer-col-3">
+                     <div class="footer-widget-title">
+                        <h4 style="white-space: nowrap;">Quick Links</h4>
+                     </div>
+                     <div class="footer-link">
+                        <ul>
+                           <li><a href="index.html">Home</a></li>
+                           <li><a href="about.html">About Us</a></li>
+                           <li><a href="team.html">Team</a></li>
+                           <li><a href="service.html">services</a></li>
+                           <li><a href="contact.html">Contacts</a></li>
+                        </ul>
+                     </div>
+                  </div>
+                  <div class="footer-widget-2 footer-col-4">
+                     <div class="footer-widget-title">
+                        <h4>Get Direction</h4>
+                     </div>
+                     <div class="theme-social">
+                        <iframe
+                           src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3885.897333377!2d77.5747370735916!3d13.105690011927289!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3bae19343ec0e885%3A0xa91cbdb822591833!2sSLV%20Prasad%20Hospital%20(A%20Unit%20of%20Sri%20Lakshmi%20Venkateshwara%20Hospitals%20Pvt%20Ltd)!5e0!3m2!1sen!2sin!4v1712049441493!5m2!1sen!2sin"
+                           width="250px" height="200px" style="border:0;" allowfullscreen="" loading="lazy"
+                           referrerpolicy="no-referrer-when-downgrade"></iframe>
+                     </div>
+                  </div>
+               </div>
+            </div>
+         </div>
+         <div class="footer-copyright-area">
+            <div class="container">
+               <div class="footer-copyright-wrapper">
+                  <div class="footer-copyright-text">
+                     <p class="mb-0">© All Copyright 2024 by <a target="_blank" href="#">Kalppa Virusha Hospital</a></p>
+                  </div>
+                  <div class="footer-conditions">
+                     <ul>
+                        <li><a href="#">Terms & Condition</a></li>
+                        <li><a href="#">Privacy Policy</a></li>
+                     </ul>
+                  </div>
+               </div>
+            </div>
+         </div>
+      </section>
+   </footer>
---
+++ header.php
@@ -0,0 +1,88 @@
+<header>
+      <div id="header-sticky" class="header-area">
+         <div class="container-fluid">
+            <div class="mega-menu-wrapper">
+               <div class="header-main">
+                  <div class="header-left">
+                     <div class="header-logo">
+                        <a href="index.html">
+                         <img src="assets/imgs/logo/KV_LOGO.webp" alt="logo not found" >
+                        </a>
+                     </div>
+                     <div class="mean__menu-wrapper d-none d-lg-block">
+                        <div class="main-menu">
+                           <nav id="mobile-menu">
+                              <ul>
+                                 <li>
+                                    <a href="index.html">Home</a>
+                                 </li>
+                                 <li>
+                                    <a href="about.html">About</a>
+                                 </li>
+                                 <li>
+                                    <a href="service.html">Services</a>
+                                 </li>
+                                 <li>
+                                    <a href="team.html">Team</a>
+                                 </li>
+                                 <li>
+                                    <a href="gallery.html">Gallery</a>
+                                 </li>
+                                 <li>
+                                    <a href="contact.html">Contact</a>
+                                 </li>
+                              </ul>
+                           </nav>
+                           <!-- for wp -->
+                           <div class="header__hamburger ml-50 d-none">
+                              <button type="button" class="hamburger-btn offcanvas-open-btn">
+                                 <span>01</span>
+                                 <span>01</span>
+                                 <span>01</span>
+                              </button>
+                           </div>
+                        </div>
+                     </div>
+                  </div>
+                  <div class="header-right d-flex align-items-center justify-content-end ">
+                     <div class="header-action d-none d-lg-inline-flex gap-3 ">
+                        <div class="link-text d-none d-xxl-block">
+                           <span><img src="assets/imgs/svg/phone-call.svg" alt=""></span>
+                           <span><a href="tel:+918861796435">+918861796435</a></span>
+                        </div>
+                        <div class="header-lang-item header-lang d-none d-xl-block">
+
+                        </div>
+                        <div style="background-color:#092A53 !important;" class="header-quick-access d-flex align-items-center">
+                         
+                           <div class="divider-line"></div>
+                           <div class="header__hamburger">
+                              <div class="sidebar__toggle">
+                                 <a class="bar-icon is-white" href="javascript:void(0)">
+                                    <span></span>
+                                    <span>
+                                       <small></small>
+                                    </span>
+                                    <span></span>
+                                 </a>
+                              </div>
+                           </div>
+                        </div>
+                     </div>
+                  </div>
+                  <div class="header__hamburger d-lg-none">
+                     <div class="sidebar__toggle">
+                        <a class="bar-icon" href="javascript:void(0)">
+                           <span></span>
+                           <span>
+                              <small></small>
+                           </span>
+                           <span></span>
+                        </a>
+                     </div>
+                  </div>
+               </div>
+            </div>
+         </div>
+      </div>
+   </header>
